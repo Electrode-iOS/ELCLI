60dd180e76a010c6486a4828219b89bd57bb5f7f
Brandon Sneed <brandon@redf.net>
2015-07-27 10:25:36 -0700

    Initial Commit

---
+++ THGCLI/THGCLI.h
@@ -0,0 +1,19 @@
+//
+//  THGCLI.h
+//  THGCLI
+//
+//  Created by Brandon Sneed on 7/27/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for THGCLI.
+FOUNDATION_EXPORT double THGCLIVersionNumber;
+
+//! Project version string for THGCLI.
+FOUNDATION_EXPORT const unsigned char THGCLIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THGCLI/PublicHeader.h>
+
+
